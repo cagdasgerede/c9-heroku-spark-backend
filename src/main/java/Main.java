e72e0d920d98c4f646d8b0cfbd573d8808ce86fe
Cagdas G <cagdas.gerede@gmail.com>
2017-09-15 23:17:10 +0000

    Dummy commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         port(8080);
-        get("/hello", (req, res) -> "Hello World !!!");
+        get("/hello", (req, res) -> "Hello Mars !!!");
     }
 }
