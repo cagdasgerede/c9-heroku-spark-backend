f75afdaadaa35593d41695d04a558f126d8b2569
Cagdas G <cagdas.gerede@gmail.com>
2017-09-15 23:27:55 +0000

    fix port assignment for heroku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,15 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        port(8080);
+        port(getHerokuAssignedPort());
         get("/hello", (req, res) -> "Hello Mars !!!");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 8080; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
