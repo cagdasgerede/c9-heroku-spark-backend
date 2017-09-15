b8b9bbec9e742f63a1d66060bfdfb752016c08f2
Cagdas G <cagdas.gerede@gmail.com>
2017-09-15 23:12:32 +0000

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        port(8080);
+        get("/hello", (req, res) -> "Hello World !!!");
+    }
+}
